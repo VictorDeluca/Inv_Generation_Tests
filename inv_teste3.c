6a1578d4ccfe59eff16b3e4eebdaa98e707ddbe7
VictorDeluca <ursheryl@hotmail.com>
2019-06-11 03:44:20 -0400

    Teste 3 (Sucesso)
    Invariantes geradas são o suficiente para realizar a verificação do código teste3.c

---
+++ inv_teste3.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+int main(){
+	int i=0, n=__VERIFIER_nondet_int();
+	__VERIFIER_assume(-i + n <= 4);
+	__VERIFIER_assume(n <= 2);
+	while(i < n){
+		i++;
+		__VERIFIER_assume(-i + n <= 4);
+		__VERIFIER_assume(n <= 2);
+	}
+	__VERIFIER_assume(n <= 2);
+	assert(i>=n);
+
+	return 0;
+}
---
+++ teste3.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+
+int main(){
+	int i=0, n=__VERIFIER_nondet_int();
+	while(i < n){
+		i++;
+	}
+	assert(i>=n);
+
+	return 0;
+}
