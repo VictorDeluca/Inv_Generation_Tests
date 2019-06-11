01602b639ecdd0a952e3a20e21750428f69c42d7
VictorDeluca <ursheryl@hotmail.com>
2019-06-11 03:51:56 -0400

    Teste 4 (Sucesso)
    As invariantes geradas foram suficientes para verificar o codigo teste4.c

---
+++ inv_teste4.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+
+int main(){
+	int i, n=__VERIFIER_nondet_int();
+	__VERIFIER_assume(-i + n <= 4);
+	__VERIFIER_assume(n <= 2);
+	for(i=0;i<n;i+=2){
+		__VERIFIER_assume(-i + n <= 4);
+		__VERIFIER_assume(n <= 2);
+	}
+	__VERIFIER_assume(n <= 2);
+	__VERIFIER_assert(i>=n);
+
+	return 0;
+}
---
+++ teste4.c
@@ -0,0 +1,10 @@
+#include <assert.h>
+
+int main(){
+	int i, n=__VERIFIER_nondet_int();
+	for(i=0;i<n;i+=2){
+	}
+	__VERIFIER_assert(i>=n);
+
+	return 0;
+}
