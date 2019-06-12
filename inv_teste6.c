f5cce6a66d93bff80b3f9aa9b535b8635bbb157a
VictorDeluca <ursheryl@hotmail.com>
2019-06-12 02:09:41 -0400

    Teste 6 (Sucesso)
    Invariantes geradas são o suficiente para realizar a verificação do código teste6.c

---
+++ inv_teste6.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+
+int main(void) {
+  unsigned int w = __VERIFIER_nondet_uint();
+  unsigned int x = w;
+  unsigned int y = __VERIFIER_nondet_uint();
+  unsigned int z = y;
+  __VERIFIER_assume(-x + y - z <= 4);
+  __VERIFIER_assume(w - x + z <= 2);
+  while (__VERIFIER_nondet_uint()) {
+    if (__VERIFIER_nondet_uint()) {
+      ++w; ++x;
+    } else {
+      --y; --z;
+    }
+    __VERIFIER_assume(-x + y - z <= 4);
+    __VERIFIER_assume(w - x + z <= 2);
+  }
+  __VERIFIER_assume(y <= 1);
+  __VERIFIER_assert(w == x && y == z);
+  return 0;
+}
