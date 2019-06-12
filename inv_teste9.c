3ab36bb71474064d2064eda590dc39726d806a4a
VictorDeluca <ursheryl@hotmail.com>
2019-06-12 04:36:58 -0400

    Teste 9 (Sucesso)
    Invariantes geradas são o suficiente para realizar a verificação do código teste9.c

---
+++ inv_teste9.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+int main() {
+    int n, sum, i;
+    n = __VERIFIER_nondet_int();
+    if (!(1 <= n && n <= 1000)) return 0;
+    sum = 0;
+    __VERIFIER_assume(-i + n - sum <= 2);
+    for(i = 1; i <= n; i++) {
+        sum = sum + i;
+	__VERIFIER_assume(-i + n - sum <= 2);
+    }
+    __VERIFIER_assume(i - sum <= 8);
+    __VERIFIER_assert(2*sum == n*(n+1));
+    return 0;
+}
+
