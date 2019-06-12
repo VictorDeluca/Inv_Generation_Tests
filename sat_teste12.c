f1287bff61986aadd6fa9d66b1800b40c37332f9
VictorDeluca <ursheryl@hotmail.com>
2019-06-12 05:51:51 -0400

    NO (Unwinding Assertion Loop)
    O teste 12 falha com tudo: Frequentemente gera 0 para todas as constantes, e das raras vezes que nao zerou tudo, as invariantes geradas nao foram suficientes para a verificacao (Testado com d_1, d_2 e d_3 <= 5, <=10, <=20 e <=100). Apenas <=10 e <=20 geraram valores diferentes de 0 para algumas constantes. Desdobrar o CFA manualmente tambem nao funcionou.

---
+++ sat_teste12.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+
+int main()
+{
+    int x = 0, y = 0;
+    
+    
+    unsigned long x0 = 0; //artificial variable for difference systems
+    // transform bound inequalities into difference inequalities by adding 
+    // x0 with the appropriate sign
+    
+
+    // Vars template    
+    int c_01 = __VERIFIER_nondet_int();       
+    __VERIFIER_assume(c_01 >= -1 && c_01 <= 1);
+    int c_02 = __VERIFIER_nondet_int();       
+    __VERIFIER_assume(c_02 >= -1 && c_02 <= 1);
+    int c_03 = __VERIFIER_nondet_int();       
+    __VERIFIER_assume(c_03 >= -1 && c_03 <= 1);
+    int c_11 = __VERIFIER_nondet_int();       
+    __VERIFIER_assume(c_11 >= -1 && c_11 <= 1);
+    int c_12 = __VERIFIER_nondet_int();
+    __VERIFIER_assume(c_12 >= -1 && c_12 <= 1);       
+    int c_13 = __VERIFIER_nondet_int();
+    __VERIFIER_assume(c_13 >= -1 && c_13 <= 1);       
+    int c_21 = __VERIFIER_nondet_int(); 
+    __VERIFIER_assume(c_21 >= -1 && c_21 <= 1);      
+    int c_22 = __VERIFIER_nondet_int();
+    __VERIFIER_assume(c_22 >= -1 && c_22 <= 1);       
+    int c_23 = __VERIFIER_nondet_int();       
+    __VERIFIER_assume(c_23 >= -1 && c_23 <= 1);
+    
+
+
+    int d_1 = __VERIFIER_nondet_int();
+    __VERIFIER_assume(d_1 >= 1 && d_1 <= 20);       
+    int d_2 = __VERIFIER_nondet_int();  
+    __VERIFIER_assume(d_2 >= 1 && d_2 <= 20);  
+    int d_3 = __VERIFIER_nondet_int();
+    __VERIFIER_assume(d_3 >= 1 && d_3 <= 20);        
+
+    assert(!(y >= 0) && !(c_01*x0 + c_11*x + c_21*y <= d_1));
+    //__VERIFIER_assume( -i + -t + -p <= -2);
+
+    assert(!(y >= 0) && !(c_01*x0 + c_11*x + c_21*y <= d_2) && !(c_02*x0 + c_12*x + c_22*y <= d_2));
+    //__VERIFIER_assume(-i + -t + -p <= -1);
+
+    assert(!(y < 0) && !(c_03*x0 + c_13*x + c_23*y <= d_3));
+    //__VERIFIER_assume(-i + -t + -p <= -1);
+
+    
+}
