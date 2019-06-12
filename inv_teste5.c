ce93cf097ae65718ab8eed799536c900e1836d74
VictorDeluca <ursheryl@hotmail.com>
2019-06-12 02:07:51 -0400

    NO (Unwinding Assertion Loop)
    Nao foi possivel verificar o codigo apenas com as invariantes geradas (Testado com --unwind 50 e valores d_1, d_2 e d_3 variados).

---
+++ inv_teste5.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+
+int main() {
+  int x=0;
+
+  __VERIFIER_assume(-x <= 3);
+  while(1)
+  {
+    __VERIFIER_assert(x==0);   
+    __VERIFIER_assume(-x <= 3); 
+  }
+  __VERIFIER_assume(-x <= 3);
+
+  __VERIFIER_assert(x!=0);
+}
---
+++ teste5.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+
+int main() {
+  int x=0;
+
+  while(1)
+  {
+    __VERIFIER_assert(x==0);    
+  }
+
+  __VERIFIER_assert(x!=0);
+}
